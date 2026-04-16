4bdca6ecf7747a2485290ce54c1bdbfc7d6234b0
190441145@ccmailg.meijo-u.ac.jp <92994706+NaCl-ringo@users.noreply.github.com>
2026-04-16 09:49:14 +0900

    審査修正1

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,21 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'dart:convert';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'dart:io';
+import 'package:app_tracking_transparency/app_tracking_transparency.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await requestTrackingPermission();
   MobileAds.instance.initialize();
-
   runApp(const MyApp());
+}
+
+Future<void> requestTrackingPermission() async {
+  final status = await AppTrackingTransparency.trackingAuthorizationStatus;
+
+  if (status == TrackingStatus.notDetermined) {
+    await AppTrackingTransparency.requestTrackingAuthorization();
+  }
 }
 
 class MyApp extends StatelessWidget {
